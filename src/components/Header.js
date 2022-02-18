7986de021fa2d15a8177ec17c074e28704a572d7
leosango <souoleo@gmail.com>
2022-02-18 17:01:20 -0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1>Componentização</h1>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Listas</li>
+            <li>Fotos</li>
+            <li>Contato</li>
+          </ul>
+        </div>
+      </div>
+    </>   
+  )
+}
+
+export default Header;
